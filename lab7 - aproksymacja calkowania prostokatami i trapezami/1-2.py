d2adbff640e111a60f7783acd73636d1d29382e9
1hubert <hrozmarynowski776@gmail.com>
2025-04-24 11:24:07 +0200

    a

--- lab7 - aproksymacja calkowania prostokatami i trapezami/1-2.py
+++ lab7 - aproksymacja calkowania prostokatami i trapezami/1-2.py
@@ -6,8 +6,8 @@ def q_rect(a, b, n, func):
     h = (b-a) / n
     a_rect = a
     b_rect = a + h
-    for i in range(n):
-        area += (b_rect-a_rect) * func((a_rect+b_rect)/2)
+    for _ in range(n):
+        area += h * func((a_rect+b_rect)/2)
 
         a_rect += h
         b_rect += h
@@ -19,13 +19,13 @@ def q_trap(a, b, n, func):
     h = (b-a) / n
     a_rect = a
     b_rect = a + h
-    for i in range(n):
-        area += (b_rect-a_rect) * ((func(a_rect)+func(b_rect))/2)
+    for _ in range(n):
+        area += h * ((func(a_rect)+func(b_rect))/2)
 
         a_rect += h
         b_rect += h
     return area
 
 
-print(q_rect(0, 2*pi, 1000, sin))
-print(q_trap(0, 2*pi, 1000, cos))
+print(f'Całka funkcji sin metodą prostokątów: {q_rect(0, 2*pi, 1000, sin)}')
+print(f'Całka funkcji cos metodą trapzów: {q_trap(0, 2*pi, 1000, cos)}')
